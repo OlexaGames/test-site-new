d76c32bfa75089c901e620a34579a72d13b10709
OlexaGames <61917242+OlexaGames@users.noreply.github.com>
2025-04-11 15:04:52 +0300

    Update js-setting.js (временное изменение)

--- instruction-DCUg-KPKrug/js-setting.js
+++ instruction-DCUg-KPKrug/js-setting.js
@@ -26,14 +26,14 @@ document.addEventListener('DOMContentLoaded', function() {
   video.play();
 });
 
-window.addEventListener('orientationchange', function() {
-  localStorage.setItem('scrollPosition', window.scrollY);
-});
+// window.addEventListener('orientationchange', function() {
+//   localStorage.setItem('scrollPosition', window.scrollY);
+// });
 
-window.addEventListener('load', function() {
-  const scrollPosition = localStorage.getItem('scrollPosition');
-  if (scrollPosition) {
-    window.scrollTo(0, scrollPosition);
-    localStorage.removeItem('scrollPosition');
-  }
-});
+// window.addEventListener('load', function() {
+//   const scrollPosition = localStorage.getItem('scrollPosition');
+//   if (scrollPosition) {
+//     window.scrollTo(0, scrollPosition);
+//     localStorage.removeItem('scrollPosition');
+//   }
+// });
