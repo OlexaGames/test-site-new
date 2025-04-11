11c9d05a55f52af3636e37d5e8311f2d0b4cecea
OlexaGames <61917242+OlexaGames@users.noreply.github.com>
2025-04-11 15:33:55 +0300

    Update orientation.js

--- instruction-DCUg-KPKrug/orientation.js
+++ instruction-DCUg-KPKrug/orientation.js
@@ -1,11 +1,27 @@
-window.addEventListener('orientationchange', function() {
-  localStorage.setItem('scrollPosition', window.scrollY);
-});
+// window.addEventListener('orientationchange', function() {
+//   localStorage.setItem('scrollPosition', window.scrollY);
+// });
 
-window.addEventListener('load', function() {
-  const scrollPosition = localStorage.getItem('scrollPosition');
-  if (scrollPosition) {
-    window.scrollTo(0, scrollPosition);
-    localStorage.removeItem('scrollPosition');
-  }
+// window.addEventListener('load', function() {
+//   const scrollPosition = localStorage.getItem('scrollPosition');
+//   if (scrollPosition) {
+//     window.scrollTo(0, scrollPosition);
+//     localStorage.removeItem('scrollPosition');
+//   }
+// });
+
+window.addEventListener('orientationchange', function() {
+  const orientation = window.orientation || 0;
+
+  const images = document.querySelectorAll('.image');
+
+  images.forEach(image => {
+    if (orientation === 90 || orientation === -90) {
+      image.style.height = 'auto';
+      image.style.width = '100%';
+    } else {
+      image.style.width = '100%';
+      image.style.height = 'auto';
+    }
+  });
 });
