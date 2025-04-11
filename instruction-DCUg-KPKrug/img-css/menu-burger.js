ae308da795c716d7695d4eb23f3a5e5bd3274aa1
OlexaGames <61917242+OlexaGames@users.noreply.github.com>
2025-04-11 14:03:54 +0300

    Add files via upload

---
+++ instruction-DCUg-KPKrug/img-css/menu-burger.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const burger = document.getElementById('burger');
+    const menu = document.getElementById('menu');
+    const body = document.body;
+
+    burger.addEventListener('click', function() {
+        menu.classList.toggle('active');
+        burger.classList.toggle('active');
+        body.classList.toggle('no-scroll');
+        burger.setAttribute('aria-expanded', burger.classList.contains('active'));
+    });
+});
